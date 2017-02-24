aec99cb9889581e6667a6a3cfcd0971c9f01a5ed
AndyObtiva <andy.am@gmail.com>
2017-02-24 14:33:46 +0400

    Refactored specs to mock out $stdout instead of relying on ticks

--- spec/lib/puts_debuggerer_spec.rb
+++ spec/lib/puts_debuggerer_spec.rb
@@ -1,25 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe 'PutsDebuggerer' do
+  before do
+    $stdout = StringIO.new
+  end
   it 'prints line number in addition to literal string object' do
-    output = `
-      ruby -e "
-        puts_debuggerer_invoker = File.expand_path(File.dirname(__FILE__) + '/spec/support/puts_debuggerer_invoker')
-        require puts_debuggerer_invoker
-        PutsDebuggererInvoker.static_greeting
-      "
-    `
+    puts_debuggerer_invoker = File.expand_path(File.dirname(__FILE__) + '/spec/support/puts_debuggerer_invoker')
+    PutsDebuggererInvoker.static_greeting
+    output = $stdout.string
     expect(output).to eq("6: \"Hello World\"\n")
   end
   it 'prints line number and ruby expression in addition to evaluated string object' do
     name = 'Muhammad'
-    output = `
-      ruby -e "
-        puts_debuggerer_invoker = File.expand_path(File.dirname(__FILE__) + '/spec/support/puts_debuggerer_invoker')
-        require puts_debuggerer_invoker
-        PutsDebuggererInvoker.dynamic_greeting('#{name}')
-      "
-    `
+    puts_debuggerer_invoker = File.expand_path(File.dirname(__FILE__) + '/spec/support/puts_debuggerer_invoker')
+    PutsDebuggererInvoker.dynamic_greeting("#{name}")
+    output = $stdout.string
     expect(output).to eq("10: \"Hello \#{name}\".inspect => \"Hello Muhammad\"\n")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
-require 'puts_debuggerer'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -23,3 +22,5 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 RSpec.configure do |config|
 
 end
+
+require 'puts_debuggerer'
