0b26658d374f7fb367ef391dca9b67f19f4d0446
AndyObtiva <andy.am@gmail.com>
2017-07-20 18:04:58 -0400

    Extracted caller backtrace spec

---
+++ spec/lib/puts_debuggerer__with_caller_backtrace__spec.rb
@@ -0,0 +1,36 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe 'PutsDebuggerer' do
+  let(:puts_debuggerer_invoker_file) {File.expand_path(File.join(__FILE__, '..', '..', 'support', 'puts_debuggerer_invoker.rb'))}
+  before do
+    $stdout = StringIO.new
+    PutsDebuggerer.print_engine = :p
+    PutsDebuggerer.formatter = nil
+    PutsDebuggerer.header = nil
+    PutsDebuggerer.footer = nil
+    PutsDebuggerer.caller = nil
+  end
+  context 'with caller backtrace' do
+    before do
+      PutsDebuggerer.caller = true
+    end
+    after do
+      PutsDebuggerer.caller = nil # defaults to false
+    end
+    it 'includes full caller backtrace when printing file, line number, ruby expression, and evaluated string object' do
+      name = 'Robert'
+      PutsDebuggererInvoker.dynamic_greeting(name)
+      output = $stdout.string
+      expected_caller = (["#{__FILE__}:#{__LINE__-2}:in `block (3 levels) in <top (required)>'"] + caller).map {|l| ' '*5 + l}
+      expect(output).to eq("[PD] #{puts_debuggerer_invoker_file}:10\n   > pd \"Hello \#{name}\"\n  => \"Hello Robert\"\n#{expected_caller.join("\n")}\n")
+    end
+    it 'includes depth-limited caller backtrace when printing file, line number, ruby expression, and evaluated string object' do
+      PutsDebuggerer.caller = 0 # just give me one backtrace entry
+      name = 'Robert'
+      PutsDebuggererInvoker.dynamic_greeting(name)
+      output = $stdout.string
+      expected_caller = ["     #{__FILE__}:#{__LINE__-2}:in `block (3 levels) in <top (required)>'"]
+      expect(output).to eq("[PD] #{puts_debuggerer_invoker_file}:10\n   > pd \"Hello \#{name}\"\n  => \"Hello Robert\"\n#{expected_caller.join("\n")}\n")
+    end
+  end
+end
--- spec/lib/puts_debuggerer_spec.rb
+++ spec/lib/puts_debuggerer_spec.rb
@@ -34,31 +34,6 @@ describe 'PutsDebuggerer' do
     expect(output).to eq("[PD] #{puts_debuggerer_invoker_file}:35\n   > greeting = \"Hello \#{pd(name)}\"\n  => \"Robert\"\n")
   end
 
-  context 'with caller backtrace' do
-    before do
-      PutsDebuggerer.caller = true
-    end
-    after do
-      PutsDebuggerer.caller = nil # defaults to false
-    end
-    it 'includes full caller backtrace when printing file, line number, ruby expression, and evaluated string object' do
-      name = 'Robert'
-      PutsDebuggererInvoker.dynamic_greeting(name)
-      output = $stdout.string
-      expected_caller = (["#{__FILE__}:#{__LINE__-2}:in `block (3 levels) in <top (required)>'"] + caller).map {|l| ' '*5 + l}
-      expect(output).to eq("[PD] #{puts_debuggerer_invoker_file}:10\n   > pd \"Hello \#{name}\"\n  => \"Hello Robert\"\n#{expected_caller.join("\n")}\n")
-    end
-    it 'includes depth-limited caller backtrace when printing file, line number, ruby expression, and evaluated string object' do
-      PutsDebuggerer.caller = 0 # just give me one backtrace entry
-      name = 'Robert'
-      PutsDebuggererInvoker.dynamic_greeting(name)
-      output = $stdout.string
-      expected_caller = ["     #{__FILE__}:#{__LINE__-2}:in `block (3 levels) in <top (required)>'"]
-      expect(output).to eq("[PD] #{puts_debuggerer_invoker_file}:10\n   > pd \"Hello \#{name}\"\n  => \"Hello Robert\"\n#{expected_caller.join("\n")}\n")
-    end
-  end
-
-
   context 'with piecemeal options' do
     before do
       load "awesome_print/core_ext/kernel.rb"
