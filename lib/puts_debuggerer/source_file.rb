cd926f3d71d1b544847793471f9712a941736439
Andy Maleh <andy.am@gmail.com>
2020-08-21 06:33:31 -0400

    Extracted source_lines from source method in SourceFile

--- lib/puts_debuggerer/source_file.rb
+++ lib/puts_debuggerer/source_file.rb
@@ -1,28 +1,31 @@
 module PutsDebuggerer
   class SourceFile  
-    def initialize(path)
-      @path = path
+    def initialize(file_path)
+      @file = File.new(file_path)
     end
     
     def source(source_line_count, source_line_number)
       @source = ''
-      f = File.new(@path)
-      if f.respond_to?(:readline) # Opal Ruby Compatibility
-        source_lines = []
+      # For Opal Ruby compatibility, skip source lines if file does not respond to readline (as in Opal)
+      lines = source_lines(source_line_count, source_line_number)
+      @source = lines.join(' '*5) if @file.respond_to?(:readline)
+      @source    
+    end
+    
+    def source_lines(source_line_count, source_line_number)
+      [].tap do |lines|
         begin
-          while f.lineno < source_line_number + source_line_count
-            file_line_number = f.lineno + 1
-            file_line = f.readline
+          while @file.lineno < source_line_number + source_line_count
+            file_line_number = @file.lineno + 1
+            file_line = @file.readline
             if file_line_number >= source_line_number && file_line_number < source_line_number + source_line_count
-              source_lines << file_line
+              lines << file_line
             end
           end
         rescue EOFError
           # Done
         end
-        @source = source_lines.join(' '*5)
       end
-      @source    
     end
   end
 end
